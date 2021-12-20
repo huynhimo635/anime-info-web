4748f69ed97bc051b12df1b6db7f7789838f2991
huynhimo635 <huynh.nguyen.imo@gmail.com>
2021-12-20 17:54:45 +0700

    edit Header

--- comps/Header.js
+++ comps/Header.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -9,9 +11,14 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Zoom from "@mui/material/Zoom";
+import Fab from "@mui/material/Fab";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+
 import Link from "next/link";
 
-import styles from "../styles/Header.module.css";
+// import styles from "../styles/Header.module.css";
 
 const pages = [
   {
@@ -24,7 +31,57 @@ const pages = [
   },
 ];
 
-const ResponsiveAppBar = () => {
+const ScrollTop = (props) => {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+};
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+
+  window: PropTypes.func,
+};
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
+const Header = (props) => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -37,88 +94,97 @@ const ResponsiveAppBar = () => {
   const logoName = "AnimeDB";
 
   return (
-    <header>
-      <AppBar position="static" className={styles.header}>
-        <Container maxWidth="xxl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h3"
-              noWrap
-              component="div"
-              sx={{ mr: 5, display: { xs: "none", md: "flex" } }}
-            >
-              {logoName}
-            </Typography>
+    <ThemeProvider theme={darkTheme}>
+      <header>
+        <AppBar position="sticky" sx={{ background: "#000" }}>
+          <Container maxWidth="xxl">
+            <Toolbar disableGutters id="back-to-top-anchor">
+              <Typography
+                variant="h3"
+                noWrap
+                component="div"
+                sx={{ mr: 5, display: { xs: "none", md: "flex" } }}
+              >
+                {logoName}
+              </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page, index) => (
+                    <MenuItem key={index} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">
+                        <Link href={page.slug}>
+                          <a>{page.name}</a>
+                        </Link>
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
               >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
+                {logoName}
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                 {pages.map((page, index) => (
-                  <MenuItem
+                  <Button
                     key={index}
                     onClick={handleCloseNavMenu}
-                    sx={{ mr: 100 }}
+                    sx={{
+                      my: 2,
+                      color: "white",
+                      display: "block",
+                      mr: 5,
+                      fontSize: "1.5rem",
+                    }}
                   >
-                    <Typography textAlign="center">
-                      <Link href={page.slug}>
-                        <a>{page.name}</a>
-                      </Link>
-                    </Typography>
-                  </MenuItem>
+                    <Link href={page.slug}>
+                      <a>{page.name}</a>
+                    </Link>
+                  </Button>
                 ))}
-              </Menu>
-            </Box>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            >
-              {logoName}
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page, index) => (
-                <Button
-                  key={index}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  <Link href={page.slug}>
-                    <a>{page.name}</a>
-                  </Link>
-                </Button>
-              ))}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </header>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <ScrollTop {...props}>
+          <Fab color="main" size="large">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </header>
+    </ThemeProvider>
   );
 };
-export default ResponsiveAppBar;
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,74 @@ import { getData } from "../redux/seasonal/seasonSlice";
 export default function Home() {
   const dispatch = useDispatch();
 
-  return <div>Sectional</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        expedita praesentium magni error pariatur delectus assumenda architecto
+        nobis odio, voluptatum, officia nesciunt. Sint magnam blanditiis iure
+        sit rerum, eaque delectus.
+      </h1>
+    </div>
+  );
 }
